fe3d374ac7ba27dcca04947e4b642e0e7eeee0e8
Tyler Ruggles <truggles@wisc.edu>
2015-05-27 23:18:58 -0400

    changed settings on fit to include 'W' which makes a HUGE difference see: https://root.cern.ch/root/html534/guides/users-guide/FittingHistograms.html#fitter-settings

--- decoFitter.py
+++ decoFitter.py
@@ -37,31 +37,43 @@ for i in range(4, 12):
     xMax.append(i * binWidth)
 print xMin
 print xMax
+#xMax = [90, 100]
 
 ''' Do some fitting to find the depth of the depletion region '''
-#funx = ROOT.TF1( 'funx', '[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', (nMax/nBins)*fitMin, nMax)
-funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 10, 70)
-#$  funx = ROOT.TF1( 'funx', '(1/(1+TMath::Exp([2]*(x-[3]))))*[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, nMax)
-f1 = gROOT.GetFunction('funx')
-f1.SetParName( 0, "vert count" )
-f1.SetParName( 1, "depth" )
-f1.SetParameter( 0, 999 )
-f1.SetParameter( 1, 999 )
-#$  f1.SetParName( 2, "steepness" )
-#$  f1.SetParameter( 2, -1 )
-#$  f1.SetParName( 3, "x offset" )
-#$  f1.SetParameter( 3, 3 )
-  
-hist.Fit('funx', 'EMRI')
-fitResult = hist.GetFunction("funx")
-fitVert = fitResult.GetParameter( 0 )
-fitDepth = fitResult.GetParameter( 1 )
-fitVertError = fitResult.GetParError( 0 )
-fitDepthError = fitResult.GetParError( 1 )
-#$  fitSteep = fitResult.GetParameter( 2 )
-#$  fitOffset = fitResult.GetParameter( 3 )
-#$  fitSteepError = fitResult.GetParError( 2 )
-#$  fitOffsetError = fitResult.GetParError( 3 )
+storage = []
+for mini in xMin:
+    for maxi in xMax:
+        #funx = ROOT.TF1( 'funx', '[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', (nMax/nBins)*fitMin, nMax)
+        funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', mini, maxi)
+        #$  funx = ROOT.TF1( 'funx', '(1/(1+TMath::Exp([2]*(x-[3]))))*[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, nMax)
+        f1 = gROOT.GetFunction('funx')
+        f1.SetParName( 0, "vert count" )
+        f1.SetParName( 1, "depth" )
+        f1.SetParameter( 0, 999 )
+        f1.SetParameter( 1, 999 )
+        #$  f1.SetParName( 2, "steepness" )
+        #$  f1.SetParameter( 2, -1 )
+        #$  f1.SetParName( 3, "x offset" )
+        #$  f1.SetParameter( 3, 3 )
+          
+        hist.Fit('funx', 'EMRIW')
+        fitResult = hist.GetFunction("funx")
+        fitVert = round(fitResult.GetParameter( 0 ), 0)
+        fitDepth = round(fitResult.GetParameter( 1 ), 3)
+        fitVertError = round(fitResult.GetParError( 0 ), 0)
+        fitDepthError = round(fitResult.GetParError( 1 ), 3)
+        #$  fitSteep = fitResult.GetParameter( 2 )
+        #$  fitOffset = fitResult.GetParameter( 3 )
+        #$  fitSteepError = fitResult.GetParError( 2 )
+        #$  fitOffsetError = fitResult.GetParError( 3 )
+        storage.append( [mini, maxi, fitVert, fitVertError, fitDepth, fitDepthError] )
+        
 
+#print storage
+for line in storage:
+    print "x range: %3i-%3i  vert: %8i+-%8i  depth: %10f+-%10f" % (line[0], line[1], line[2], line[3], line[4], line[5])
 
 gROOT.cd()
+
+
+
