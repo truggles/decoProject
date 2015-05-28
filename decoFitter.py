c7da1ea4543c4d6ef4ec64b06652c8e3f18bec3d
Tyler Ruggles <truggles@wisc.edu>
2015-05-28 12:03:41 -0400

    Nice output histos that show the fit region in red

--- decoFitter.py
+++ decoFitter.py
@@ -15,11 +15,19 @@ p.add_argument("file", nargs=1,
 args = p.parse_args()
 filename = args.file[0]
 
+fitCode = 'EMRISWW'
+#fitCode = 'EMRISW'
+#fitCode = 'EMRIS'
+
 ifile = ROOT.TFile("%s" % filename, "r")
 if 'HTC' in filename:
     histName = "HTC Wildfire Slength"
+    saveName = "HTC_Wildfire_S"
+    titleName = "HTC_Wildfire_S"
 if 'SPH' in filename:
     histName = "Samsung Galaxy S2length"
+    saveName = "Samsung_Galaxy_S2"
+    titleName = "Samsung_Galaxy_S2"
 hist = ifile.Get(histName)
 
 binWidth = int( hist.GetBinWidth(1) )
@@ -35,9 +43,10 @@ for i in range(1, 4):
 xMax = []
 for i in range(4, 12):
     xMax.append(i * binWidth)
+xMin = [10]
+#xMax = [90, 100]
 print xMin
 print xMax
-#xMax = [90, 100]
 
 ''' Do some fitting to find the depth of the depletion region '''
 storage = []
@@ -57,7 +66,7 @@ for mini in xMin:
         #$  f1.SetParName( 3, "x offset" )
         #$  f1.SetParameter( 3, 3 )
           
-        rslt = hist.Fit('funx', 'EMRISW')
+        rslt = hist.Fit('funx', fitCode)
         fitResult = hist.GetFunction("funx")
         fitVert = round(fitResult.GetParameter( 0 ), 0)
         fitDepth = round(fitResult.GetParameter( 1 ), 3)
@@ -68,13 +77,36 @@ for mini in xMin:
         #$  fitSteepError = fitResult.GetParError( 2 )
         #$  fitOffsetError = fitResult.GetParError( 3 )
         accurate = rslt.IsValid()
+        ''' store results for a nice print out summary '''
         storage.append( [mini, maxi, fitVert, fitVertError, fitDepth, fitDepthError, accurate] )
-        
+        print "\n"
+
+        ''' save histos with fit '''
+        funNew = ROOT.TF1( 'funNew', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, Max)
+        f2 = gROOT.GetFunction('funNew')
+        f2.SetParameter( 0, fitVert )
+        f2.SetParameter( 1, fitDepth )
+
+        c1 = ROOT.TCanvas("c1","title",400,400)
+        c1.cd ()
+        hist.Draw('hist e1')
+        f2.Draw('same')
+        fitResult.Draw('same')
+        fitResult.SetLineColor(ROOT.kRed)
+        hist.SetMaximum( fitVert * 1.2 )
+        hist.SetTitle('%s Fit x range: %i-%i' % (titleName, mini, maxi))
+        c1.SaveAs('fits/%s_%s_%i-%i.png' % (saveName, fitCode, mini, maxi) )
+        c1.Close()
 
 #print storage
-for line in storage:
-    print "x range: %3i-%3i  vert: %8i+-%8i  depth: %10f+-%10f   accurate: %7s" % (line[0], line[1], line[2], line[3], line[4], line[5], line[6])
+ofile = open('fits/%s_%s.txt' % (saveName, fitCode), 'w')
 
+for line in storage:
+    line =  "x range: %3i-%3i  vert: %8i+-%8i  depth: %10f+-%10f   accurate: %7s" % (line[0], line[1], line[2], line[3], line[4], line[5], line[6])
+    print line
+    ofile.write("%s\n" % line)
+
+ofile.close()
 gROOT.cd()
 
 
