27ac1f2f52a0bdfd2963922c137c26abc7b1675d
Tyler Ruggles <truggles@wisc.edu>
2015-06-07 15:09:04 +0200

    Added dl/dTheta to fitter function which adds (1/(1+x^2)) and makes the function converge which is great, and the right thing to do

--- decoFitter.py
+++ decoFitter.py
@@ -58,12 +58,14 @@ for fitCode in ['EMRISWW', 'EMRISW', 'EMRIS']:
     print xMax
     
     ''' Do some fitting to find the depth of the depletion region '''
+    ''' I previously forgot the dl / dTheta, that is the new 1/(1+x^2) component '''
     storage = []
     for mini in xMin:
         for maxi in xMax:
             print "x range: %i-%i" % (mini, maxi)
             #funx = ROOT.TF1( 'funx', '[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', (nMax/nBins)*fitMin, nMax)
-            funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', mini, maxi)
+            #funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', mini, maxi)
+            funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )*( 1 / ( 1 + ((x*x)/([1]*[1]))))', mini, maxi)
             #$  funx = ROOT.TF1( 'funx', '(1/(1+TMath::Exp([2]*(x-[3]))))*[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, nMax)
             f1 = gROOT.GetFunction('funx')
             f1.SetParName( 0, "vert count" )
@@ -91,7 +93,7 @@ for fitCode in ['EMRISWW', 'EMRISW', 'EMRIS']:
             print "\n"
     
             ''' save histos with fit '''
-            funNew = ROOT.TF1( 'funNew', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, Max)
+            funNew = ROOT.TF1( 'funNew', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )*( 1 / ( 1 + ((x*x)/([1]*[1]))))', 0, Max)
             f2 = gROOT.GetFunction('funNew')
             f2.SetParameter( 0, fitVert )
             f2.SetParameter( 1, fitDepth )
