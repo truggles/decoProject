b35734c9a421cf6687541693cb1b9965bd8e8201
Tyler Ruggles <truggles@wisc.edu>
2015-06-11 10:00:37 +0200

    Matt's update + updated values for total yield, Matt had the total number, we only what the number in the fit range, right?

--- decoFitter.py
+++ decoFitter.py
@@ -18,8 +18,8 @@ filename = args.file[0]
 #fitCode = 'EMRISWW'
 #fitCode = 'EMRISW'
 fitCode = 'EMRIS'
-isotropic = False
 #isotropic = True
+isotropic = False
 
 #for fitCode in ['EMRISWW', 'EMRISW', 'EMRIS']:
 ifile = ROOT.TFile("%s" % filename, "r")
@@ -34,11 +34,13 @@ if 'HTC' in filename:
     saveName = "HTC_Wildfire_S"
     titleName = "HTC Wildfire S"
     abrev = 'HTC'
+    nEvents = 256*10 # events * 10 pixels per bin
 if 'SPH' in filename:
     histName = "Samsung Galaxy S2length"
     saveName = "Samsung_Galaxy_S2"
     titleName = "Samsung Galaxy S2"
     abrev = 'SPH'
+    nEvents = 131*7 # events * 7 pixels per bin
 hist = ifile.Get(histName)
 
 binWidth = int( hist.GetBinWidth(1) )
@@ -62,23 +64,27 @@ print xMax
 ''' Do some fitting to find the depth of the depletion region '''
 ''' I previously forgot the dl / dTheta, that is the new 1/(1+x^2) component '''
 storage = []
-folder = 'fits%s%s' % (abrev, num)
+folder = './fits%s%s' % (abrev, num)
 if isotropic: folder = folder + 'iso'
 for mini in xMin:
     for maxi in xMax:
         print "x range: %i-%i" % (mini, maxi)
-        #funx = ROOT.TF1( 'funx', '[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', (nMax/nBins)*fitMin, nMax)
-        #funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', mini, maxi)
-        #funx = ROOT.TF1( 'funx', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )*( 1 / ([1]*( 1 + ((x*x)/([1]*[1])))))', mini, maxi)
-        #if isotropic: funx = ROOT.TF1( 'funx', '[0]*sin( TMath::ATan( x / [1]) )*( 1 / ([1]*( 1 + ((x*x)/([1]*[1])))))', mini, maxi)
-        funx = ROOT.TF1( 'funx', '([0] * x * [1]*[1]*[1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1])*(x*x+[1]*[1]) )', mini, maxi)
-        if isotropic: funx = ROOT.TF1( 'funx', '([0] * x * [1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1]) )', mini, maxi)
-        #$  funx = ROOT.TF1( 'funx', '(1/(1+TMath::Exp([2]*(x-[3]))))*[0] * cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )', 0, nMax)
+        if isotropic:
+            norm = '( (2*%s)/([1]*[1]) )*( ( (%s*%s+[1]*[1])*(%s*%s+[1]*[1]) )/(%s*%s-%s*%s) )' % (nEvents,mini,mini,maxi,maxi,maxi,maxi,mini,mini)  
+            fit_function = norm+'*(x*[1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1]) )'
+        else:
+            norm = '( (4*%s)/([1]*[1]*[1]*[1]) )*( ( (%s*%s+[1]*[1])*(%s*%s+[1]*[1])*(%s*%s+[1]*[1])*(%s*%s+[1]*[1]) )/( (%s*%s+[1]*[1])*(%s*%s+[1]*[1]) - (%s*%s+[1]*[1])*(%s*%s+[1]*[1]) ) )' % (nEvents,mini,mini,maxi,maxi,mini,mini,maxi,maxi,maxi,maxi,maxi,maxi,mini,mini,mini,mini) 
+            fit_function = norm+'*(x * [1]*[1]*[1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1])*(x*x+[1]*[1]) )' 
+        
+        print('norm', norm)
+        print('func', fit_function)
+        funx = ROOT.TF1( 'funx', fit_function, mini, maxi)
         f1 = gROOT.GetFunction('funx')
-        f1.SetParName( 0, "scale" )
+        f1.SetParName( 0, "scale" ) # Fit no longer depends on this parameter, but leaving it here in case we change things
         f1.SetParName( 1, "depth" )
         f1.SetParameter( 0, 999 )
-        f1.SetParameter( 1, 999 )
+        f1.SetParameter( 1, 10 )
+        #f1.FixParameter(0,norm) # 
         #$  f1.SetParName( 2, "steepness" )
         #$  f1.SetParameter( 2, -1 )
         #$  f1.SetParName( 3, "x offset" )
@@ -90,6 +96,7 @@ for mini in xMin:
         fitDepth = round(fitResult.GetParameter( 1 ), 3)
         fitScaleError = round(fitResult.GetParError( 0 ), 0)
         fitDepthError = round(fitResult.GetParError( 1 ), 3)
+        
         #$  fitSteep = fitResult.GetParameter( 2 )
         #$  fitOffset = fitResult.GetParameter( 3 )
         #$  fitSteepError = fitResult.GetParError( 2 )
@@ -97,10 +104,9 @@ for mini in xMin:
         accurate = rslt.IsValid()
 
         ''' save histos with fit '''
-        #funNew = ROOT.TF1( 'funNew', '[0]*cos( TMath::ATan( x / [1]) )*cos( TMath::ATan( x / [1]) )*( 1 / ([1]*( 1 + ((x*x)/([1]*[1])))))', 0, Max)
-        #if isotropic: funNew = ROOT.TF1( 'funNew', '[0]*sin( TMath::ATan( x / [1]) )*( 1 / ([1]*( 1 + ((x*x)/([1]*[1])))))', 0, Max)
-        funNew = ROOT.TF1( 'funNew', '([0] * x * [1]*[1]*[1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1])*(x*x+[1]*[1]) )', mini, maxi)
-        if isotropic: funNew = ROOT.TF1( 'funNew', '([0] * x * [1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1]) )', mini, maxi)
+        #funNew = ROOT.TF1( 'funNew', '([0] * x * [1]*[1]*[1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1])*(x*x+[1]*[1]) )', mini, maxi)
+        #if isotropic: funNew = ROOT.TF1( 'funNew', '([0] * x * [1]*[1])/( (x*x+[1]*[1])*(x*x+[1]*[1]) )', mini, maxi)
+        funNew = ROOT.TF1( 'funNew', fit_function, mini, maxi)
         f2 = gROOT.GetFunction('funNew')
         f2.SetParameter( 0, fitScale )
         f2.SetParameter( 1, fitDepth )
@@ -116,8 +122,9 @@ for mini in xMin:
         fitResult.SetLineColor(ROOT.kRed)
 
         chiSq = hist.Chisquare( funNew )
-
+        
         ''' Build Legend '''
+        #gStyle.SetOptFit(1) # stats box (pcev) p=probability, c=chi_sq/dof, e=errors, v=name/vals parameters 
         hist.SetStats(0)
         legend = ROOT.TPaveStats(Max - 5*binWidth, hist.GetMaximum()*0.88, Max, hist.GetMaximum()*1.14 )
         chiSq_ = str( round( chiSq, 3 ) )
